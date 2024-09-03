3a8f00cce098711ae2797a7d69ed16f7c5c9e314
Degesh <60864803+thiyageshvenkat@users.noreply.github.com>
2024-09-02 19:20:05 -0700

    Create Seafarers.py

---
+++ Seafarers.py
@@ -0,0 +1,50 @@
+import random
+tiles = list("🌊" * 19 + "🌾" * 5 + "🗻" * 4 + "🧱" * 4 + "🐑" * 5 + "🌳" * 5)
+random.shuffle(tiles)
+board = []
+for i in range(7):
+    board.append([])
+    for j in range(7):
+        board[i].append(' ')
+for i in range(5):
+    board[0][1 + i] = tiles.pop(0)
+for i in range(6):
+    board[1][1 + i] = tiles.pop(0)
+for i in range(7):
+    board[2][i] = tiles.pop(0)
+for i in range(6): ##### THE MIDDLE ONE WITH THE BUILT IN OCEAN TILES
+    board[3][1 + i] = tiles.pop(0)
+for i in range(7):
+    board[4][i] = tiles.pop(0)
+for i in range(6):
+    board[5][1 + i] = tiles.pop(0)
+for i in range(5):
+    board[6][1 + i] = tiles.pop(0)
+print("\nThe top-most row of the map displayed represents the longer edge of the Seafarers map. \nThe middle row of the map displayed contains only 6 tiles, because the first and last two tiles of the eight tiles of that row must be ocean.")
+print("Catan Seafarers Board: \n")
+for i in range(len(board)):
+    if i % 2 == 1:
+        print(" ", *board[i])
+    else:
+        print("  ", *board[i])
+numbers = [2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 8, 8, 9, 9, 9, 10, 10, 10, 11, 11, 12]
+numbers = [str(i) for i in numbers] 
+random.shuffle(numbers)
+board_numbers = board
+for i in range(len(board_numbers)):
+    for j in range(len(board_numbers[i])):
+        if board_numbers[i][j] != "🌊" and board_numbers[i][j] != " ":
+            board_numbers[i][j] = numbers.pop(0)
+        elif board_numbers[i][j] == "🌊":
+            board_numbers[i][j] = "~"
+print("\n")
+print("Numbers:")
+for i in range(len(board_numbers)):
+    if i % 2 == 1:
+        print(" ")
+        for j in board_numbers[i]:
+            print(j.ljust(3), end = "")
+    else:
+        print("  ")
+        for j in board_numbers[i]:
+            print(j.ljust(3), end = "")
